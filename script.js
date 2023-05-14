352a005192babd1d6bfcdce7e5e5ccc0d8629463
ogdd <olgudolu@gmail.com>
2023-05-14 21:40:12 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+var button0 = document.getElementById('box0Button');
+button0.addEventListener('click', function() {
+  console.log('Button clicked!');
+  box0Node = document.getElementById('box0')
+  box0Node.style.backgroundColor = 'white'
+var button1 = document.getElementById('box1Button');
+button1.addEventListener('click', function () {
+    console.log('Why are you doing this!');
+    box1Node = document.getElementById('box1')
+    box1Node.style.backgroundColor = 'white';
+var button2 = document.getElementById('box2Button')
+button2.addEventListener('click', function() {
+    console.log('You are killing us!!')
+    box2Node = document.getElementById('box2')
+    box2Node.style.backgroundColor = 'white'
+})
+})
+});
+
